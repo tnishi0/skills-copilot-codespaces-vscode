dc6ec910c943bcc44e91cc0e6f4c49073a0997e0
tnishi0 <tnishi0@users.noreply.github.com>
2023-08-11 23:08:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    return member;
+}
